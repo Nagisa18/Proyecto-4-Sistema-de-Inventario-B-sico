735592eb9edb42d2de4813b5dcc808a4bf650dcd
[valentina] <valentina04vega@gmail.com>
2026-03-28 08:22:17 -0500

    feat: implementar clase Producto y metodo agregar, TAREA by LUIS FELIPE RAMOS

--- src/inventory.py
+++ src/inventory.py
@@ -1,1 +1,17 @@
- 
+class Producto:
+    def __init__(self, nombre, cantidad, precio):
+        self.nombre = nombre
+        self.cantidad = cantidad
+        self.precio = precio
+
+    def __str__(self):
+        return f"Producto: {self.nombre} | Stock: {self.cantidad} | Precio: ${self.precio}"
+
+class Inventario:
+    def __init__(self):
+        self.productos = []
+
+    def agregar_producto(self, nombre, cantidad, precio):
+        nuevo_producto = Producto(nombre, cantidad, precio)
+        self.productos.append(nuevo_producto)
+        print(f"Producto '{nombre}' agregado con éxito.")
